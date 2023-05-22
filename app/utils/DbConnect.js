20dd94a31562f5ea9954a00f8154c12972c5f416
sadique-cws <sadique.hussain96@gmail.com>
2023-05-22 18:13:00 +0530

    Initial commit from Create Next App

---
+++ app/utils/DbConnect.js
@@ -0,0 +1,8 @@
+const DbConnect = (req,res) => {
+    if(mongoose.connection.readyState >= 1){
+        return mongoose.connection;
+    }
+    return mongoose.connect(process.env.DB_URL)
+}
+
+export default DbConnect;
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    experimental: { appDir: true ,    serverComponentsExternalPackages: ["mongoose"]},
+
+      webpack(config) {
+        config.experiments = { ...config.experiments, topLevelAwait: true }
+        return config
+  },
+}
